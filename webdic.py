e2f2b00f5629116b7e8f5e54d5e23264834cecc2
0x4E0x650x6F <tiago.alexand@gmail.com>
2015-12-02 11:30:10 +0000

    performance improvment

--- webdic.py
+++ webdic.py
@@ -6,7 +6,9 @@ import sys
 import unicodedata
 from multiprocessing import Pool
 from string import ascii_lowercase
-from urllib2 import urlopen, URLError, HTTPError
+from urllib2 import urlopen, Request, URLError, HTTPError
+
+pagination_re = re.compile(r"<p>(\d+) .* - (\d+) .*</p>")
 
 
 def get_page(args):
@@ -18,7 +20,9 @@ def get_page(args):
     url = base_url % (length, charecter, page_number)
     print "[*]\t%s" % url
     try:
-        response = urlopen(url)
+        request = Request(url=url)
+        response = urlopen(request, timeout=10)
+        print "[*]\tGot responce from %s" % url
         html = response.read().decode('iso-8859-1').encode('utf8')
         return html
     except HTTPError, e:
@@ -27,35 +31,46 @@ def get_page(args):
     except URLError, e:
         print '[*]\tWe failed to reach a server.'
         print '[*]\tReason: ', e.reason
+    except Exception, e:
+        raise e
 
 
-def main(length):
-    pool = Pool(processes=3)
-    try:
-        for charecter in ascii_lowercase:
-            words = []
-            fname = "pt-%s-%s" % (length, charecter)
-            pagination_re = re.compile(r"<p>(\d+) .* - (\d+) .*</p>")
-            html = get_page((charecter, length, 1))
-            fwords = parse_words(html)
-            if fwords:
-                words.extend(fwords)
-            number_pages_match = pagination_re.search(html)
-            num_pages = number_pages_match.group(1)
-            num_total_words = int(number_pages_match.group(2))
-            if num_pages > 1:
-                page_mapping = [(charecter, length, index) for index in range(2, int(num_pages) + 1)]
-                results = pool.map(get_page, page_mapping)
-                for html in results:
-                    fwords = parse_words(html)
-                    if fwords:
-                        words.extend(fwords)
-            print "[*]\ttotal words found %s total words expected %s" % (len(words), num_total_words)
-            save_data(words, fname)
-    except KeyboardInterrupt:
-        pool.terminate()
-        print "[*]\nYou cancelled the program!"
-        sys.exit(1)
+def get_words(args):
+    return parse_words(get_page(args))
+
+
+def main(length, initial_char):
+        char_indx = ascii_lowercase.find(initial_char)
+        for charecter in ascii_lowercase[char_indx:]:
+            pool = Pool(processes=3)
+            try:
+                words = []
+                fname = "pt-%s-%s" % (length, charecter)
+                html = get_page((charecter, length, 1))
+                fwords = parse_words(html)
+                if fwords:
+                    words.extend(fwords)
+                number_pages_match = pagination_re.search(html)
+                num_pages = number_pages_match.group(1)
+                num_total_words = int(number_pages_match.group(2))
+                if num_pages > 1:
+                    page_mapping = [(charecter, length, index) for index in range(2, int(num_pages) + 1)]
+                    results = pool.map(get_words, page_mapping)
+                    print"[*]\tAppending resutls to list"
+                    for fwords in results:
+                        if fwords:
+                            words.extend(fwords)
+                print "[*]\ttotal words found %s total words expected %s" % (len(words), num_total_words)
+                save_data(words, fname)
+                pool.close()
+                pool.join()
+            except KeyboardInterrupt:
+                pool.terminate()
+                sys.exit(1)
+                print "[*]\nYou cancelled the program!"
+            finally:
+                pool.close()
+                pool.terminate()
 
 
 def save_data(word_lst, filename):
@@ -73,8 +88,12 @@ def parse_words(html):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print "[*]\targs missing script.py length"
+    if len(sys.argv) != 3:
+        print "[*]\targs missing script.py length initial char", len(sys.argv)
         sys.exit(1)
-    length = sys.argv[1]
-    main(length)
+    length, init_char = sys.argv[1:]
+
+    try:
+        main(length, init_char)
+    except KeyboardInterrupt:
+        sys.exit(0)
